0ee8023d2339bf72a6bb5a24b1a5d9ad74046e3a
floj <flo.juettner@gmail.com>
2020-07-29 11:03:46 +0200

    allow encoding from stdin

--- README.md
+++ README.md
@@ -4,11 +4,21 @@ Simple URL encoder and decoder for the commandline
 
 ## Usage
 ```sh
+# encode
 > urlenc "https://github.com/floj/urlenc"
 https%3A%2F%2Fgithub.com%2Ffloj%2Furlenc
 
+# decode
 > urlenc -d "https%3A%2F%2Fgithub.com%2Ffloj%2Furlenc"
 https://github.com/floj/urlenc
+
+# encode from stdin
+> echo -n "https://github.com/floj/urlenc" | urlenc
+https%3A%2F%2Fgithub.com%2Ffloj%2Furlenc
+
+# decode from stdin
+> echo -n "https%3A%2F%2Fgithub.com%2Ffloj%2Furlenc" | urlenc -d
+https%3A%2F%2Fgithub.com%2Ffloj%2Furlenc
 ```
 
 ## Build
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/url"
 	"os"
 )
@@ -22,10 +23,25 @@ func main() {
 	decode := flag.Bool("d", false, "Decode")
 	flag.Parse()
 
-	args := flag.CommandLine.Args()
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	var args []string
+	if info.Mode()&os.ModeCharDevice == 0 {
+		in, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not read from stdin\n")
+			os.Exit(1)
+		}
+		args = []string{string(in)}
+	} else {
+		args = flag.CommandLine.Args()
+	}
 
 	exitcode := 0
-
 	var op func(string) (string, error) = escape
 	var opLabel = "encode"
 	if *decode {
@@ -36,7 +52,7 @@ func main() {
 	for _, a := range args {
 		v, err := op(a)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Could not %s %s: %v", opLabel, trimToLen(a, 10), err)
+			fmt.Fprintf(os.Stderr, "Could not %s %s: %v\n", opLabel, trimToLen(a, 10), err)
 			exitcode = 1
 			continue
 		}
